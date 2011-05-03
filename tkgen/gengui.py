92395fdb08a0fabe2017ea8086cfe738dd2c9704
Thijs Metsch <tmetsch@opensolly.(none)>
2011-05-03 18:13:35 +0200

    pep8 compliancy

--- tkgen/gengui.py
+++ tkgen/gengui.py
@@ -15,6 +15,14 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301  USA
+#
+"""
+Module for easy creation of Tkinter GUIs.
+
+Created on Apr 21, 2011
+
+@author: tmetsch
+"""
 
 from Tkconstants import W, E, N, S
 from Tkinter import Tk, IntVar, StringVar
@@ -22,16 +30,11 @@ from Tkinter import Tk, IntVar, StringVar
 import json
 import Tkinter
 
-'''
-Simple class which wraps Tk and uses some JSON to contruct a GUI...
-
-Created on Apr 21, 2011
-
-@author: tmetsch
-'''
-
 
 class Generator(object):
+    """
+    Simple class which wraps Tk and uses some JSON to contruct a GUI.
+    """
 
     menu = None
 
@@ -45,9 +48,9 @@ class Generator(object):
         self.root.title(title)
 
         ui_file = open(filename)
-        ui = json.load(ui_file)
+        user_interface = json.load(ui_file)
 
-        self.create_widgets(self.root, ui)
+        self.create_widgets(self.root, user_interface)
 
         return self.root
 
@@ -57,7 +60,9 @@ class Generator(object):
         """
         for name in items.keys():
             current = items[name]
-            if isinstance(current, dict) and not self._contains_list(current) and not self._contains_dict(current):
+            if isinstance(current,
+                          dict) and not self._contains_list(
+                              current) and not self._contains_dict(current):
                 self._create_widget(name, parent, current)
 
             elif isinstance(current, dict) and self._contains_list(current):
@@ -71,6 +76,8 @@ class Generator(object):
             elif isinstance(current, list):
                 for item in current:
                     self.create_widgets(parent, {name: item})
+
+    # pylint: disable-msg=W0142,R0201
 
     def _contains_dict(self, items):
         """
@@ -109,7 +116,7 @@ class Generator(object):
         parent -- The parent widget.
         desc -- Dictionary containing the description for this widget.
         """
-        row, column, columnspan, rowweight, colweight, options = self._get_options(desc)
+        row, column, c_span, r_weight, c_weight, opt = self._get_options(desc)
 
         try:
             widget_factory = getattr(Tkinter, name)
@@ -118,26 +125,27 @@ class Generator(object):
                 import ttk
                 widget_factory = getattr(ttk, name)
             except AttributeError:
-                raise AttributeError('Neither Tkinter nor ttk have a widget named: ', name)
+                raise AttributeError('Neither Tkinter nor ttk have a' +
+                                     ' widget named: ', name)
 
-        widget = widget_factory(parent, **options)
+        widget = widget_factory(parent, **opt)
 
         widget.grid(row=row,
                     column=column,
-                    columnspan=columnspan,
+                    columnspan=c_span,
                     sticky=N + E + W + S,
                     padx=2,
                     pady=2)
 
         # propaget size settings when needed.
-        if 'width' in options or 'height' in options:
+        if 'width' in opt or 'height' in opt:
             widget.grid_propagate(0)
 
         # set parent weight of the cells
-        if rowweight > 0:
-            parent.rowconfigure(row, weight=rowweight)
-        if colweight > 0:
-            parent.columnconfigure(column, weight=colweight)
+        if r_weight > 0:
+            parent.rowconfigure(row, weight=r_weight)
+        if c_weight > 0:
+            parent.columnconfigure(column, weight=c_weight)
 
         return widget
 
@@ -174,7 +182,8 @@ class Generator(object):
             rowweight = dictionary['weight']
             dictionary.pop('weight')
         for key in dictionary.keys():
-            if not isinstance(dictionary[key], dict) and not isinstance(dictionary[key], list):
+            if not isinstance(dictionary[key],
+                              dict) and not isinstance(dictionary[key], list):
                 options[str(key)] = str(dictionary[key])
             elif isinstance(dictionary[key], list) and key == key.lower():
                 # so we have an attribute list...
@@ -227,14 +236,14 @@ class Generator(object):
         name -- Name of the Checkbox.
         focus -- indicates wether this item has the focus.
         """
-        c = IntVar()
+        var = IntVar()
         item = self._find_by_name(self.root, name)
-        item.config(variable = c)
+        item.config(variable=var)
 
         if focus:
             item.focus_set()
-        
-        return c
+
+        return var
 
     def entry(self, name, key=None, cmd=None, focus=False):
         """
@@ -242,21 +251,21 @@ class Generator(object):
 
         name -- Name of the Tk widget.
         key -- Needed if a key should be bound to this instance.
-        cmd -- If key is defined cmd needs to be defined - will be triggered when the key is pressed.
+        cmd -- If key is defined cmd needs to be defined.
         focus -- Indicates wether this entry should take focus.
         """
-        v = StringVar()
+        var = StringVar()
 
         item = self._find_by_name(self.root, name)
-        item.config(textvariable = v)
+        item.config(textvariable=var)
 
         if focus:
             item.focus_set()
-        
+
         if key is not None and cmd is not None:
             item.bind(key, cmd)
-        
-        return v
+
+        return var
 
     def label(self, name):
         """
@@ -264,10 +273,10 @@ class Generator(object):
 
         name -- Name of the Label.
         """
-        v = StringVar()
+        var = StringVar()
         item = self._find_by_name(self.root, name)
-        item.config(textvariable=v)
-        return v
+        item.config(textvariable=var)
+        return var
 
     def toplevel(self, filename, title='Dialog'):
         """
@@ -283,44 +292,20 @@ class Generator(object):
         self.create_widgets(dialog, dialog_def)
         dialog.grid()
 
-    def notebook(self, parent, filename, name='Tab'):
-        """
-        Add a tab to a tkk notebook widget.
-
-        parent -- The parent notebook widget instance.
-        filename -- The file which describes the content of the tab.
-        name -- The name of the tab.
-        """
-        frame = Tkinter.Frame()
-        ui_file = open(filename)
-        tab1_def = json.load(ui_file)
-        self.create_widgets(frame, tab1_def)
-        parent.add(frame, text=name)
-
-    def treeview(self, treeview, name, values, parent='', index=0):
-        """
-        Adds an item to a treeview.
-
-        treeview -- The treeview to add the items to.
-        name -- The name of the value.
-        values -- The values itself.
-        parent -- Default will create root items, specify a parent to create a leaf.
-        index -- If index < current # of items - insert at the top (Default: 0).
-        """
-        return treeview.insert(parent, index, text=name, values=values)
-
     def find(self, name):
         """
         Find a Tk widget by name and return it.
         """
         result = self._find_by_name(self.root, name)
         if result is None:
-            raise KeyError('Tkinter widget with name "' + name + '" not found.')
+            raise KeyError('Tkinter widget with name "' +
+                           name + '" not found.')
         return result
 
     def create_menu(self, commands, name=None, parent=None, popup=False):
         """
-        Creates a menu(entry) if non is available. Returns the created menu so you can define submenus.
+        Creates a menu(entry) if non is available. Returns the created menu so
+        you can define submenus.
 
         commands -- dict with 'label':'command' structure.
         name -- Needs to be provided if it is a dropdown or submenu.
@@ -359,3 +344,31 @@ class Generator(object):
             return tmp_menu
         else:
             raise AttributeError('Invalid parameters provided')
+
+    # Move?
+
+    def notebook(self, parent, filename, name='Tab'):
+        """
+        Add a tab to a tkk notebook widget.
+
+        parent -- The parent notebook widget instance.
+        filename -- The file which describes the content of the tab.
+        name -- The name of the tab.
+        """
+        frame = Tkinter.Frame()
+        ui_file = open(filename)
+        tab1_def = json.load(ui_file)
+        self.create_widgets(frame, tab1_def)
+        parent.add(frame, text=name)
+
+    def treeview(self, treeview, name, values, parent='', index=0):
+        """
+        Adds an item to a treeview.
+
+        treeview -- The treeview to add the items to.
+        name -- The name of the value.
+        values -- The values itself.
+        parent -- Default will create root items, specify a parent to create a leaf.
+        index -- If index < current # of items - insert at the top (Default: 0).
+        """
+        return treeview.insert(parent, index, text=name, values=values)
